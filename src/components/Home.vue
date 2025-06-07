153bf65e58cfec72b57c9e3882ea53b670bbd8ab
joacs <gaelmzl49@gmail.com>
2025-06-06 21:29:00 -0500

    Realizacion del componente visual del dinero con cambios en el layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,17 +3,50 @@
         <template #header>
             <Header></Header>
         </template>
+
+        <template #resume>
+            <div class="resume">
+                <Resume
+                    :total-label="'Ahorro total'"
+                    :label="label"
+                    :total-amount="1000000"
+                    :amount="totalAmount"
+                > 
+                    <template #graphic>
+                        Graphic
+                    </template>
+
+                    <template #action>
+                        Action
+                    </template>
+                </Resume>
+            </div>
+        </template>
+
+        <template #movements>
+            <Movements/>
+        </template>
     </Layout>
 </template>
 
 <script>
     import Layout from "@/components/Layout.vue";
     import Header from "@/components/Header.vue";
+    import Resume from "@/components/resume/Index.vue";
+    import Movements from "@/components/Movements.vue";
 
     export default {
         components: {
             Layout,
-            Header
-        }
+            Header,
+            Resume,
+            Movements
+        },
+        data() {
+            return {
+                label: null,
+                amount: null, 
+            };
+        },
     }
 </script>
