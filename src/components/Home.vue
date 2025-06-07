5ca8439c55f0945cc1dbba6d25f7ced7fbfdf6ed
Joaquin <124814537+Joacs49@users.noreply.github.com>
2025-06-06 22:16:56 -0500

    Cambios en el layout, estilos y emision de evento para eliminar campo especifico del historial (#2)
    * Se agrego en el README el uso de defineAsynComponent
    
    * Se agrego el Layout y su documentacion
    
    * Realizacion del componente visual del dinero con cambios en el layout
    
    * Realizacion del historial
    
    * Emision del evento para poder borrar y estilos para dinero dnegativo o positivo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,17 +3,58 @@
         <template #header>
             <Header></Header>
         </template>
+
+        <template #resume>
+            <div class="resume">
+                <Resume
+                    :total-label="'Ahorro total'"
+                    :label="label"
+                    :total-amount="1000000"
+                    :amount="totalAmount"
+                > 
+                    <template #graphic>
+                        Graphic
+                    </template>
+
+                    <template #action>
+                        Action
+                    </template>
+                </Resume>
+            </div>
+        </template>
+
+        <template #movements>
+            <Movements
+                :movements="movement"
+            />
+        </template>
     </Layout>
 </template>
 
 <script>
     import Layout from "@/components/Layout.vue";
     import Header from "@/components/Header.vue";
+    import Resume from "@/components/resume/Index.vue";
+    import Movements from "@/components/movements/Index.vue";
 
     export default {
         components: {
             Layout,
-            Header
-        }
+            Header,
+            Resume,
+            Movements
+        },
+        data() {
+            return {
+                label: null,
+                amount: null, 
+                movement: [
+                    { id: 0, title: 'Movimiento 1' , description: "Descripción del movimiento 1" , amount: 1000 },
+                    { id: 1, title: 'Movimiento 2', description: "Descripción del movimiento 2", amount: 2000 },
+                    { id: 2, title: 'Movimiento 3', description: "Descripción del movimiento 3", amount: 3000 },
+                    { id: 3, title: 'Movimiento 4', description: "Descripción del movimiento 4", amount: 4000 },
+                ],
+            };
+        },
     }
 </script>
